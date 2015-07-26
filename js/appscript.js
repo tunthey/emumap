b7d72a2045277c3493b50e02edee2166a39d9ddc
Tunde Aderinwale <aderinwaletunde@gmail.com>
2015-07-26 12:43:39 +0300

    updating and testing

--- js/appscript.js
+++ js/appscript.js
@@ -9,7 +9,7 @@ var watchID;
 
         var directionsDisplay;
         var directionsService = new google.maps.DirectionsService();
-        //var map;
+        var map;
 
         // position options
         var MAXIMUM_AGE = 200; // miliseconds
@@ -90,6 +90,139 @@ function where_im_i(){
             } else {
                 alert('Geolocation not supported.')
             }
+}
+
+function load_search_cafeteria() {
+    if($('#cafe_name_search').val()=='')
+        alert('Please enter the name of cafeteria to search');
+    else{
+        var cafe_name = $('#cafe_name_search').val();
+        var dataurl = 'http://localhost:81/emumapservice/list_search_cafeteria.php?name='+cafe_name;
+    $.ajax({
+                url: dataurl,
+                //timeout: 5000,
+                beforeSend: function() {
+                $.mobile.loading('show', {
+                 theme: "a",
+                 text: "Processing Request... please wait",
+                 textonly: true,
+                 textVisible: true
+                    }); 
+                },
+                error: function(jqXHR, textStatus) {
+                    console.log(textStatus);
+                    $.mobile.loading('hide');
+                    redirect_to('#errorpage');
+                },
+
+                fail: function(jqXHR, textStatus, errorThrown){
+                    $.mobile.loading('hide');
+                    console.log(textStatus);
+                    redirect_to('#errorpage');
+                },
+
+                success: function (jsondata) {
+                    $.mobile.loading('hide');
+                    if(jsondata.length>0){
+                    $('#cafeteria_content_listing_area').html('<ul data-theme="b" data-role="listview">');
+                       for (var i = 0; i < jsondata.length; i++) {
+                       $('#cafeteria_content_listing_area').append('<li data-theme="b"><a href="#" onclick="load_cafeteria_details('+jsondata[i].cafeteria_list.id+')">'+jsondata[i].cafeteria_list.name+'</a></li>');
+                       }
+                       $('#cafeteria_content_listing_area').append('</ul>');
+                         $('#cafeteria_content_listing_area').listview().listview('refresh');
+                    redirect_to('#cafeteria_list');
+                    }
+                    else
+                        alert('No Record Found!');
+                }
+
+
+            });
+    }
+}
+
+function load_all_cafeteria(){
+    var dataurl = 'http://localhost:81/emumapservice/list_all_cafeteria.php';
+    $.ajax({
+                url: dataurl,
+                //timeout: 5000,
+                beforeSend: function() {
+                $.mobile.loading('show', {
+                 theme: "a",
+                 text: "Processing Request... please wait",
+                 textonly: true,
+                 textVisible: true
+                    });
+                },
+                error: function(jqXHR, textStatus) {
+                    console.log(textStatus);
+                    $.mobile.loading('hide');
+                    redirect_to('#errorpage');
+                },
+
+                fail: function(jqXHR, textStatus, errorThrown){
+                    $.mobile.loading('hide');
+                    console.log(textStatus);
+                    redirect_to('#errorpage');
+                },
+
+                success: function (jsondata) {
+                    $.mobile.loading('hide');
+                    $('#cafeteria_content_listing_area').html('<ul data-theme="b" data-role="listview">');
+                       for (var i = 0; i < jsondata.length; i++) {
+                       $('#cafeteria_content_listing_area').append('<li data-theme="b"><a href="#" onclick="load_cafeteria_details('+jsondata[i].cafeteria_list.id+')">'+jsondata[i].cafeteria_list.name+'</a></li>');
+                      console.log();
+                       }
+                       $('#cafeteria_content_listing_area').append('</ul>');
+                         $('#cafeteria_content_listing_area').listview().listview('refresh');
+                    redirect_to('#cafeteria_list');
+                }
+
+
+            });
+}
+
+function load_cafeteria_details(id){
+     var dataurl = 'http://localhost:81/emumapservice/get_cafeteria_details.php?id='+id;
+    $.ajax({
+                url: dataurl,
+                //timeout: 5000,
+                beforeSend: function() {
+                $.mobile.loading('show', {
+                 theme: "a",
+                 text: "Loading Cafeteria Details.. please wait",
+                 textonly: true,
+                 textVisible: true
+                    });
+                },
+                error: function(jqXHR, textStatus) {
+                    console.log(textStatus);
+                    $.mobile.loading('hide');
+                    redirect_to('#errorpage');
+                },
+
+                fail: function(jqXHR, textStatus, errorThrown){
+                    $.mobile.loading('hide');
+                    console.log(textStatus);
+                    redirect_to('#errorpage');
+                },
+
+                success: function (jsondata) {
+                    $.mobile.loading('hide');
+                    $('#cafeteria_details_area').html('');
+                       for (var i = 0; i < jsondata.length; i++) {
+                       $('#cafeteria_details_area').append('<img src="'+jsondata[i].cafeteria_list.cover_pics+'" alt="Cafeteria Cover Picture" class="banner"><p>'+jsondata[i].cafeteria_list.brief_info+'</p><button class="ui-btn" onclick="load_direction('+jsondata[i].cafeteria_list.lat+','+jsondata[i].cafeteria_list.lng+')" >Get Direction</button>');
+                       }
+                      // $('#cgdb').button();
+                    redirect_to('#cafeteria_details');
+                }
+
+
+            });
+}
+
+function load_direction(lat,lng){
+    
 }
 
 function download_manual(){
